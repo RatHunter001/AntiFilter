ce4e22e0dde5f64c9f0aa2f22f88c3f79feca332
RatHunter001 <43678100+RatHunter001@users.noreply.github.com>
2020-06-08 20:03:49 +0300

    Add project files.

---
+++ Anti MCAT/Form1.Designer.vb
@@ -0,0 +1,145 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form, bileşen listesini temizlemeyi bırakmayı geçersiz kılar.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form Tasarımcısı tarafından gerektirilir
+    Private components As System.ComponentModel.IContainer
+
+    'NOT: Aşağıdaki yordam Windows Form Tasarımcısı için gereklidir
+    'Windows Form Tasarımcısı kullanılarak değiştirilebilir.  
+    'Kod düzenleyicisini kullanarak değiştirmeyin.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.SuspendLayout()
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.ForeColor = System.Drawing.SystemColors.WindowText
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(586, 167)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Button1.Location = New System.Drawing.Point(12, 185)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(282, 51)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "TRANSLATE"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Special Characters", "Russian Characters"})
+        Me.ComboBox1.Location = New System.Drawing.Point(97, 420)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(134, 21)
+        Me.ComboBox1.TabIndex = 3
+        Me.ComboBox1.Tag = "1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label1.Location = New System.Drawing.Point(13, 416)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(78, 25)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Type : "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(528, 440)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(70, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "dsf001#1337"
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.ForeColor = System.Drawing.SystemColors.WindowText
+        Me.RichTextBox2.Location = New System.Drawing.Point(12, 242)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.Size = New System.Drawing.Size(586, 167)
+        Me.RichTextBox2.TabIndex = 6
+        Me.RichTextBox2.Text = ""
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Button2.Location = New System.Drawing.Point(316, 185)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(282, 51)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "COPY"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(473, 440)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(49, 13)
+        Me.LinkLabel1.TabIndex = 8
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "v4gugsP"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(610, 462)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.RichTextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximumSize = New System.Drawing.Size(626, 501)
+        Me.MinimumSize = New System.Drawing.Size(626, 501)
+        Me.Name = "Form1"
+        Me.Text = "Anti MC:AT"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents RichTextBox2 As RichTextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents LinkLabel1 As LinkLabel
+End Class
---
+++ Anti MCAT/Form1.vb
@@ -0,0 +1,158 @@
+﻿
+Public Class Form1
+    Private aa As String
+    Private b As String
+    Private c As String
+    Private d As String
+    Private ee As String
+    Private f As String
+    Private g As String
+    Private h As String
+    Private i As String
+    Private j As String
+    Private k As String
+    Private l As String
+    Private m As String
+    Private n As String
+    Private o As String
+    Private p As String
+    Private r As String
+    Private s As String
+    Private t As String
+    Private u As String
+    Private v As String
+    Private y As String
+    Private z As String
+
+    Private Sub RichTextBox1_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If ComboBox1.SelectedIndex = 1 Then
+            Me.aa = "α"
+            Me.b = "б"
+            Me.c = "ƈ"
+            Me.d = "Ð"
+            Me.ee = "ё"
+            Me.f = "ƒ"
+            Me.g = "ğ"
+            Me.h = "ɧ"
+            Me.i = "į"
+            Me.j = "J"
+            Me.k = "ĸ"
+            Me.l = "ʟ"
+            Me.m = "ṁ"
+            Me.n = "л"
+            Me.o = "ö"
+            Me.p = "ρ"
+            Me.r = "ṙ"
+            Me.s = "š"
+            Me.t = "т"
+            Me.u = "ü"
+            Me.v = "ᴠ"
+            Me.y = "Ý"
+            Me.z = "ᴢ"
+        ElseIf ComboBox1.SelectedIndex = 0 Then
+            Me.aa = "Ä"
+            Me.b = "ß"
+            Me.c = "ƈ"
+            Me.d = "Ð"
+            Me.ee = "є"
+            Me.f = "ƒ"
+            Me.g = "Ğ"
+            Me.h = "ɧ"
+            Me.i = "į"
+            Me.j = "J"
+            Me.k = "ĸ"
+            Me.l = "ʟ"
+            Me.m = "ᴹ"
+            Me.n = "Ѝ"
+            Me.o = "Ő"
+            Me.p = "ρ"
+            Me.r = "Ṝ"
+            Me.s = "Ƨ"
+            Me.t = "Ƚ"
+            Me.u = "ü"
+            Me.v = "ᴠ"
+            Me.y = "Ý"
+            Me.z = "ᴢ"
+        Else
+            MsgBox("aa")
+        End If
+
+        Try
+            Me.RichTextBox2.Text = Me.RichTextBox1.Text
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("A", Me.aa)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("a", Me.aa)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("B", Me.b)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("b", Me.b)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("C", Me.c)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("c", Me.c)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("D", Me.d)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("d", Me.d)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("E", Me.ee)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("e", Me.ee)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("F", Me.f)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("f", Me.f)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("G", Me.g)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("g", Me.g)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("H", Me.h)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("h", Me.h)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("İ", Me.i)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("i", Me.i)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("J", Me.j)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("j", Me.j)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("K", Me.k)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("k", Me.k)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("L", Me.l)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("l", Me.l)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("M", Me.m)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("m", Me.m)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("N", Me.n)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("n", Me.n)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("O", Me.o)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("o", Me.o)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("P", Me.p)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("p", Me.p)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("R", Me.r)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("r", Me.r)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("S", Me.s)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("s", Me.s)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("T", Me.t)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("t", Me.t)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("U", Me.u)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("u", Me.u)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("V", Me.v)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("v", Me.v)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("Y", Me.y)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("y", Me.y)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("Z", Me.z)
+            Me.RichTextBox2.Text = Me.RichTextBox2.Text.Replace("z", Me.z)
+        Catch exception As System.Exception
+            MessageBox.Show(exception.Message)
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBox1.SelectedIndex = My.Settings.abbas
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        My.Settings.abbas = ComboBox1.SelectedIndex
+        My.Settings.Save()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            Clipboard.SetText(RichTextBox2.Text)
+        Catch ex As Exception
+            MsgBox("Write something you idiot")
+        End Try
+
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("https://discord.gg/v4gugsP")
+    End Sub
+End Class
